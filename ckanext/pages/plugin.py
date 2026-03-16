6400034e80e7552c73973129e48a153b56fca167
debannen <debannen@igbmc.fr>
2026-03-16 15:10:36 +0100

    fix: add back public directory registrations for CKEditor assets in PagesPlugin

--- ckanext/pages/plugin.py
+++ ckanext/pages/plugin.py
@@ -113,6 +113,11 @@ class PagesPlugin(PagesPluginBase):
 
         tk.add_resource('assets', 'pages')
 
+        tk.add_public_directory(config, 'assets/')
+        tk.add_public_directory(config, 'assets/vendor/ckeditor/')
+        tk.add_public_directory(config, 'assets/vendor/ckeditor/skins/moono-lisa')
+
+
     def get_helpers(self):
         return {
             'build_nav_main': build_pages_nav_main,
